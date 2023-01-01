e201268115723f4e7513171aa03f0b5de69b3b36
dionizio <gabriel.dionizio@picpay.com>
2023-01-01 12:56:44 -0300

    Feature: adding test to entity

--- entity/task_test.go
+++ entity/task_test.go
@@ -1,1 +1,23 @@
 package entity
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTask(t *testing.T) {
+	task := NewTask("Title Task", "Desc Task", uuid.New(), uuid.New(), GetTaskInitialState())
+	assert.Equal(t, task.Title, "Title Task")
+	assert.Equal(t, task.Description, "Desc Task")
+	assert.Equal(t, task.Status, GetTaskInitialState())
+	assert.NotNil(t, task.Id)
+	assert.NotNil(t, task.UserId)
+	assert.NotNil(t, task.ManagerUserId)
+}
+
+func TestGetTaskState(t *testing.T) {
+	assert.Equal(t, GetTaskInitialState(), "IN_PROGRESS")
+	assert.Equal(t, GetTaskFinalState(), "CONCLUDED")
+}
--- entity/user_test.go
+++ entity/user_test.go
@@ -1,1 +1,19 @@
 package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user := NewUser("Dionizio", GetUserRoleTechnician())
+	assert.Equal(t, user.Name, "Dionizio")
+	assert.Equal(t, user.Role, GetUserRoleTechnician())
+	assert.NotNil(t, user.Id)
+}
+
+func TestUserRole(t *testing.T) {
+	assert.Equal(t, GetUserRoleManager(), "MANAGER")
+	assert.Equal(t, GetUserRoleTechnician(), "TECHNICIAN")
+}
